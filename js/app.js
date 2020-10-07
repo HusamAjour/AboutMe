ada0a09cf0e6fa473584af3b357eda86e367aa8c
HusamAjour <husamaj278@gmail.com>
2020-10-07 14:38:02 +0300

    Modify question 7: 6 tries instead of 7

--- js/app.js
+++ js/app.js
@@ -5,150 +5,148 @@ var name = prompt('Hi there stranger. What is your name?');
 alert('Welcome ' + name + '. Let\'s play a guessing game! I\'m going to ask you questions about my self and you need to guess the answers. Let\'s start :) !');
 
 // Question 1
-function question1(){
-var morningPerson = prompt('Do you think I\'m a morning person?');
-if(morningPerson.toLowerCase() === 'yes' || morningPerson.toLowerCase() === 'y'){
-  console.log('That is wrong. I\'m not a morning person.');
-  alert('That is wrong. I\'m not a morning person.');
-} else if(morningPerson.toLowerCase() === 'no' || morningPerson.toLowerCase() === 'n'){
-  console.log('That is correct. I\'m not a morning person.');
-  alert('That is correct. I\'m not a morning person.');
-  resultConter++;
-} else{
-  console.log('Wrong input.');
-  alert('Wrong input.');
-}
+function question1() {
+  var morningPerson = prompt('Do you think I\'m a morning person?');
+  if (morningPerson.toLowerCase() === 'yes' || morningPerson.toLowerCase() === 'y') {
+    console.log('That is wrong. I\'m not a morning person.');
+    alert('That is wrong. I\'m not a morning person.');
+  } else if (morningPerson.toLowerCase() === 'no' || morningPerson.toLowerCase() === 'n') {
+    console.log('That is correct. I\'m not a morning person.');
+    alert('That is correct. I\'m not a morning person.');
+    resultConter++;
+  } else {
+    console.log('Wrong input.');
+    alert('Wrong input.');
+  }
 }
 question1();
 // Question 2
 
-function question2(){
-var coffee = prompt('Do you think I\'m a coffee addict?');
-if(coffee.toLowerCase() === 'yes' || coffee.toLowerCase() === 'y'){
-  console.log('That is correct. I\'m a coffee addict.');
-  alert('That is correct. I\'m a coffee addict.');
-  resultConter++;
-} else if(coffee.toLowerCase() === 'no' || coffee.toLowerCase() === 'n'){
-  console.log('That is correct. I\'m a coffee addict');
-  alert('That is correct. I\'m a coffee addict');
-} else{
-  console.log('Wrong input.');
-  alert('Wrong input.');
-}
+function question2() {
+  var coffee = prompt('Do you think I\'m a coffee addict?');
+  if (coffee.toLowerCase() === 'yes' || coffee.toLowerCase() === 'y') {
+    console.log('That is correct. I\'m a coffee addict.');
+    alert('That is correct. I\'m a coffee addict.');
+    resultConter++;
+  } else if (coffee.toLowerCase() === 'no' || coffee.toLowerCase() === 'n') {
+    console.log('That is correct. I\'m a coffee addict');
+    alert('That is correct. I\'m a coffee addict');
+  } else {
+    console.log('Wrong input.');
+    alert('Wrong input.');
+  }
 }
 question2();
 // Question 3
 
-function question3(){
-var airplane = prompt('Do you think I\'ve ever been on an airplane?');
-if(airplane.toLowerCase() === 'yes' || airplane.toLowerCase() === 'y'){
-  console.log('That is wrong. I\'ve never been on an airplane.');
-  alert('That is wrong. I\'ve never been on an airplane.');
-} else if(airplane.toLowerCase() === 'no' || airplane.toLowerCase() === 'n'){
-  console.log('That is correct. I\'ve never been on an airplane.');
-  resultConter++;
-  alert('That is correct. I\'ve never been on an airplane.');
-} else{
-  console.log('Wrong input.');
-  alert('Wrong input.');
-}
+function question3() {
+  var airplane = prompt('Do you think I\'ve ever been on an airplane?');
+  if (airplane.toLowerCase() === 'yes' || airplane.toLowerCase() === 'y') {
+    console.log('That is wrong. I\'ve never been on an airplane.');
+    alert('That is wrong. I\'ve never been on an airplane.');
+  } else if (airplane.toLowerCase() === 'no' || airplane.toLowerCase() === 'n') {
+    console.log('That is correct. I\'ve never been on an airplane.');
+    resultConter++;
+    alert('That is correct. I\'ve never been on an airplane.');
+  } else {
+    console.log('Wrong input.');
+    alert('Wrong input.');
+  }
 }
 question3();
 // Question 4
 
-function question4(){
-var license = prompt('Do you think I have a driving license?');
-if(license.toLowerCase() === 'yes' || license.toLowerCase() === 'y'){
-  console.log('That is wrong. I don\'t a driving license');
-  alert('That is wrong. I don\'t a driving license');
-} else if(license.toLowerCase() === 'no' || license.toLowerCase() === 'n'){
-  console.log('That is correct. I don\'t a driving license');
-  resultConter++;
-  alert('That is correct. I don\'t a driving license');
-} else{
-  console.log('Wrong input.');
-  alert('Wrong input.');
-}
+function question4() {
+  var license = prompt('Do you think I have a driving license?');
+  if (license.toLowerCase() === 'yes' || license.toLowerCase() === 'y') {
+    console.log('That is wrong. I don\'t a driving license');
+    alert('That is wrong. I don\'t a driving license');
+  } else if (license.toLowerCase() === 'no' || license.toLowerCase() === 'n') {
+    console.log('That is correct. I don\'t a driving license');
+    resultConter++;
+    alert('That is correct. I don\'t a driving license');
+  } else {
+    console.log('Wrong input.');
+    alert('Wrong input.');
+  }
 }
 question4();
 // Question 5
 
-function question5(){
-var tennis = prompt('Do you think I play tennis?');
-if(tennis.toLowerCase() === 'yes' || tennis.toLowerCase() === 'y'){
-  console.log('That is wrong. I don\'t a play tennis.');
-  alert('That is wrong. I don\'t a play tennis.');
-} else if(tennis.toLowerCase() === 'no' || tennis.toLowerCase() === 'n'){
-  console.log('That is correct. I don\'t a play tennis.');
-  alert('That is correct. I don\'t a play tennis.');
-  resultConter++;
-}else{
-  console.log('Wrong input.');
-  alert('Wrong input.');
-}
+function question5() {
+  var tennis = prompt('Do you think I play tennis?');
+  if (tennis.toLowerCase() === 'yes' || tennis.toLowerCase() === 'y') {
+    console.log('That is wrong. I don\'t a play tennis.');
+    alert('That is wrong. I don\'t a play tennis.');
+  } else if (tennis.toLowerCase() === 'no' || tennis.toLowerCase() === 'n') {
+    console.log('That is correct. I don\'t a play tennis.');
+    alert('That is correct. I don\'t a play tennis.');
+    resultConter++;
+  } else {
+    console.log('Wrong input.');
+    alert('Wrong input.');
+  }
 }
 question5();
 // Question 6
 
-function question6(){
-var numberToGuess = 8;
-alert('Ok ' + name + ', let\'s play another game! This game is called Guess The number. You have 4 tries to guess the number. Let\'s start!');
-var guessGameScore = false;
-for (var i = 0; i < 4; i++) {
-  var guessTry = parseInt(prompt('Guess No.' + i + 1));
-  if (guessTry === numberToGuess) {
-    alert('Correct guess. Congratulations!');
-    resultConter++;
-    guessGameScore = true;
-    break;
-  } else if (guessTry > numberToGuess) {
-    alert('Too high');
-  } else if (guessTry < numberToGuess) {
-    alert('Too low');
-  } else {
-    alert('Wrong input');
-  }
-}
-
-if(guessGameScore === true)
-{
-  alert('Congratulatins! You guessed the number correctly. The number is ' + numberToGuess);
-} else {
-  alert('Game over. You didn\'t guess the number :( . The number is ' + numberToGuess );
-}
-}
-question6();
-// Question 7
-function question7(){
-var favoriteFootballers = ['kaka', 'kroos', 'ronaldo', 'lampard', 'zidane', 'cristiano', 'ronaldinho'];
-var footballerGuessBolean = false;
-alert('Ok ' + name + '. Another game to play. Try to guess 1 of my favorite 7 footballers of all times. You have 6 guesses. Let\'s start!');
-
-for(var j = 0; j < 7; j++){
-  var footballerGuess = prompt('Guess No.' + (j + 1)).toLocaleLowerCase();
-  for(var k=0; k < favoriteFootballers.length; k++){
-    if(footballerGuess === favoriteFootballers[k]){
-      alert('Correct! ' + footballerGuess + ' is one of my favorite footballers.');
-      footballerGuessBolean = true;
+function question6() {
+  var numberToGuess = 8;
+  alert('Ok ' + name + ', let\'s play another game! This game is called Guess The number. You have 4 tries to guess the number. Let\'s start!');
+  var guessGameScore = false;
+  for (var i = 0; i < 4; i++) {
+    var guessTry = parseInt(prompt('Guess No.' + i + 1));
+    if (guessTry === numberToGuess) {
+      alert('Correct guess. Congratulations!');
       resultConter++;
+      guessGameScore = true;
       break;
+    } else if (guessTry > numberToGuess) {
+      alert('Too high');
+    } else if (guessTry < numberToGuess) {
+      alert('Too low');
+    } else {
+      alert('Wrong input');
     }
   }
 
-  if(footballerGuessBolean === true){
-    break;
-  }
-  else{
-    alert('Wrong answer. Try again.');
+  if (guessGameScore === true) {
+    alert('Congratulatins! You guessed the number correctly. The number is ' + numberToGuess);
+  } else {
+    alert('Game over. You didn\'t guess the number :( . The number is ' + numberToGuess);
   }
 }
+question6();
+// Question 7
+function question7() {
+  var favoriteFootballers = ['kaka', 'kroos', 'ronaldo', 'lampard', 'zidane', 'cristiano', 'ronaldinho'];
+  var footballerGuessBolean = false;
+  alert('Ok ' + name + '. Another game to play. Try to guess 1 of my favorite 7 footballers of all times. You have 6 guesses. Let\'s start!');
 
-if(footballerGuessBolean === true)
-{
-  alert('Congratulatins! You guessed one of my favorite footballer correctly. Here is a list of my favortie 7 footballers. ' + favoriteFootballers);
-} else {
-  alert('Game over. You didn\'t guess any of my favorite footballers :( .  Here is a list of my favortie 7 footballers. ' + favoriteFootballers);
-}
+  for (var j = 0; j < 6; j++) {
+    var footballerGuess = prompt('Guess No.' + (j + 1)).toLocaleLowerCase();
+    for (var k = 0; k < favoriteFootballers.length; k++) {
+      if (footballerGuess === favoriteFootballers[k]) {
+        alert('Correct! ' + footballerGuess + ' is one of my favorite footballers.');
+        footballerGuessBolean = true;
+        resultConter++;
+        break;
+      }
+    }
+
+    if (footballerGuessBolean === true) {
+      break;
+    }
+    else {
+      alert('Wrong answer. Try again.');
+    }
+  }
+
+  if (footballerGuessBolean === true) {
+    alert('Congratulatins! You guessed one of my favorite footballer correctly. Here is a list of my favortie 7 footballers. ' + favoriteFootballers);
+  } else {
+    alert('Game over. You didn\'t guess any of my favorite footballers :( .  Here is a list of my favortie 7 footballers. ' + favoriteFootballers);
+  }
 }
 question7();
 // Result
